ce7706def80be5d1f93f148bf04965f3c089f847
Alexandre ROBERT <a.robert@weinmanntech.lan>
2023-02-21 13:38:59 +0100

    v2.2

--- copie_differentielle_de_fichier/Program.cs
+++ copie_differentielle_de_fichier/Program.cs
@@ -2,7 +2,9 @@
  * Alexandre Robert
  * 01/12/2022
  * 
- * Version 2.1.3 - Upgraded log with execution time
+ * Version 2.2 Added suppression of files in destination folder if they don't exist in source folder
+ * 
+ * v2.2.1 : AppConfig, folder.ini et log : chemin automatique
  * 
  * COPIE DE DOSSIERS ET FICHIERS AVEC CONFIGURATION
  * 
@@ -12,22 +14,21 @@
  */
 
 using System.Xml;
-// Import du fichier App.Config.xml (spécifier son chemin)
+
+// Import de la configuration
 XmlDocument AppConfig = new XmlDocument();
 AppConfig.Load("K:\\08_Informatique\\15_Scripts\\Sauvegarde\\copie_differentielle_de_fichier\\App.Config.xml");
-// Définition des valeurs selon le fichier
 XmlNodeList source = AppConfig.GetElementsByTagName("source");
 XmlNodeList destination = AppConfig.GetElementsByTagName("destination");
-string sourceValue = source[0].InnerText;
-string destinationValue = destination[0].InnerText;
-
-// Liste des répertoires à copier à partir du fichier folder.ini (spécifier son chemin)
-string[] folders = File.ReadAllLines("K:\\08_Informatique\\15_Scripts\\Sauvegarde\\copie_differentielle_de_fichier\\folder.ini");
 
 //declaration des variables globales
+string repertoireSource = source[0].InnerText;
+string repertoireDestination = destination[0].InnerText;
 int nbFichiersModifier = 0;
 int nbFichierIgnore = 0;
+int nbFichierSupprime = 0;
 int nbErreur = 0;
+string[] liste_dossier_a_copier = File.ReadAllLines("K:\\08_Informatique\\15_Scripts\\Sauvegarde\\copie_differentielle_de_fichier\\folder.ini");
 List<string> listeErreur = new List<string>();
 string log = "K:\\08_Informatique\\15_Scripts\\Sauvegarde\\copie_differentielle_de_fichier\\logs\\" + DateTime.Now.ToString("yyyy-MM-dd") + ".log";
 const string SEPARATION = "--------------------------------------------------";
@@ -37,15 +38,15 @@ const string SEPARATION = "--------------------------------------------------";
 
 //affichage infos
 Console.WriteLine("Informations définies dans le fichier App.Config.xml : \n");
-Console.WriteLine("SOURCE : " + sourceValue);
-Console.WriteLine("DESTINATION : " + destinationValue + "\n" + SEPARATION + "\n");
+Console.WriteLine("SOURCE : " + repertoireSource);
+Console.WriteLine("DESTINATION : " + repertoireDestination + "\n" + SEPARATION + "\n");
 Console.WriteLine("La copie concerne l'entièreté des fichiers contenus dans les répertoires suivants :\n");
-foreach (string folder in folders)
+foreach (string dossier_principal in liste_dossier_a_copier)
 {
-    Console.WriteLine(folder);
+    Console.WriteLine(dossier_principal);
 }
 
-// Choix de la méthode de vérification des fichier avant copie
+// vérification des fichier avant copie
 VerifDesFichiers();
 
 // Choix de la méthode de copie
@@ -53,128 +54,91 @@ Console.WriteLine("\nChoix de la méthode de copie :");
 Console.WriteLine("1 - Copie simple");
 Console.WriteLine("2 - Copie différentielle");
 int choixMethode = Convert.ToInt32(Console.ReadLine());
+
+// Choix de la suppression
+Console.WriteLine("\nVoulez-vous supprimer les fichiers dans le répertoire de destination qui ne sont pas présents dans le répertoire source ? (O/N)");
+string choixSuppression = Console.ReadLine();
+bool suppression_est_demandee = false;
+if (choixSuppression == "O" || choixSuppression == "o")
+{
+    suppression_est_demandee = true;
+}
+
 File.AppendAllText(log, "****************************************************************" + Environment.NewLine);
 
-// Effectue la copie selon le choix (simple/différentielle)
-if (choixMethode == 1)
+// Effectue la copie selon le choix
+switch (choixMethode)
 {
-    FaireCopyDirectory();
-}
-else if (choixMethode == 2)
-{
-    FaireCopyDirectoryDiff();
-}
-else
-{
-    Console.WriteLine("Choix incorrect");
+    case 1:
+        FaireCopieSimple();
+        break;
+    case 2:
+        FaireCopieDifferentielle();
+        break;
+    default:
+        Console.WriteLine("Choix incorrect");
+        break;
 }
 
 //****************** METHODES ******************//
 
-// Affichage erreurs
-void AfficherErreur(int nbErreur, string log)
-{
-    if (nbErreur > 0)
-    {
-        File.AppendAllText(log, "Nombre d'erreur constatées : " + nbErreur + Environment.NewLine);
-        foreach (string erreur in listeErreur)
-        {
-            File.AppendAllText(log, erreur + Environment.NewLine);
-        }
-    }
-    else
-    {
-        File.AppendAllText(log, "Aucune erreur constatée" + Environment.NewLine);
-    }
-}
-
 // Répète la méthode de copie simple pour chaque répertoire
-void FaireCopyDirectory()
+void FaireCopieSimple()
 {
-    Console.WriteLine("Vous vous apprêter à faire un copie simple des répertoires susmentionnés dans le répertoire suivant : \n" + destinationValue + "\n");
+    Console.WriteLine("Vous vous apprêtez à faire un copie simple des répertoires susmentionnés dans le répertoire suivant : \n" + repertoireDestination + "\n");
     AppuyezPourContinuer();
     DateTime dateDebut = DateTime.Now;
     File.AppendAllText(log, "Début de la copie simple :" + DateTime.Now + Environment.NewLine);
     
-    //pour chaque répertoire
-    foreach (string folder in folders)
+    foreach (string dossier in liste_dossier_a_copier)
     {
         try
         {
-            //afficher le répertoire à copier
-            Console.WriteLine(folder);
-            //copie le répertoire et ses sous-répertoires
-            CopyDirectory(sourceValue + folder, destinationValue + folder);
+            CopieSimple(repertoireSource + dossier, repertoireDestination + dossier);
+            if (suppression_est_demandee) SupprimerFichiers(repertoireSource + dossier, repertoireDestination + dossier);
         }
-        //erreur si l'accès au dossier n'est pas autorisé
         catch (Exception e)
         {
-            Console.WriteLine("Erreur : " + e.Message);
-            //ajout de cette erreur dans le fichier log et dans la liste d'erreur
             string time = DateTime.Now.ToString();
             nbErreur++;
             listeErreur.Add(e.Message + " " + time);
         }
     }
-
-    AfficherErreur(nbErreur, log);
-    Console.WriteLine("\nTache terminee \n\nInformations dans le fichier log : \n" + log);
-
-    //ajout des infos dans le fichier log
-    File.AppendAllText(log, "Fin: " + DateTime.Now + Environment.NewLine);
-    File.AppendAllText(log, SEPARATION + Environment.NewLine);
-    File.AppendAllText(log, "Liste des répertoires concernés : " + Environment.NewLine);
-    foreach (string folder in folders) { File.AppendAllText(log, sourceValue + folder + Environment.NewLine); }
-    File.AppendAllText(log, SEPARATION + Environment.NewLine);
-    File.AppendAllText(log, "Copie simple terminée : \nNombre de fichiers copiés : " 
-        + nbFichiersModifier + Environment.NewLine + "\nTemps d'éxecution : " 
-        + (DateTime.Now - dateDebut).ToString(@"hh\:mm\:ss") + Environment.NewLine);
+    
+    Afficher_erreur(nbErreur, log);
+    RemplirLog(dateDebut, log);
 }
 
 //répète la méthode de copie différentielle pour chaque répertoire
-void FaireCopyDirectoryDiff()
+void FaireCopieDifferentielle()
 {
-    Console.WriteLine("Vous vous apprêter à faire un copie différentielle des répertoires susmentionnés dans le répertoire suivant : \n" + destinationValue + "\n");
+    Console.WriteLine("Vous vous apprêtez à faire un copie différentielle des répertoires susmentionnés dans le répertoire suivant : \n" + repertoireDestination + "\n");
     AppuyezPourContinuer();
     DateTime dateDebut = DateTime.Now;
     File.AppendAllText(log, "Début de la copie différentielle :" + dateDebut + Environment.NewLine);
-    
-    //pour chaque répertoire
-    foreach (string folder in folders)
+
+    foreach (string dossier in liste_dossier_a_copier)
     {
         try
         {
-            //afficher le répertoire à copier
-            Console.WriteLine(folder);
-            //copie le répertoire et ses sous-répertoires
-            CopyDirectoryDiff(sourceValue + folder, destinationValue + folder);
+            CopieDifferentielle(repertoireSource + dossier, repertoireDestination + dossier);
+            if (suppression_est_demandee) SupprimerFichiers(repertoireSource + dossier, repertoireDestination + dossier); 
         }
-        //erreur si l'accès au dossier n'est pas autorisé
+        
         catch (Exception e)
         {
-            Console.WriteLine("Erreur : " + e.Message);
-            //ajout de cette erreur dans le fichier log et dans la liste d'erreur
             string time = DateTime.Now.ToString();
             nbErreur++;
             listeErreur.Add(e.Message + " " + time);
         }
     }
 
-    AfficherErreur(nbErreur, log);
-    Console.WriteLine("\nTache terminee \n\nInformations dans le fichier log : \n" + log);
-    
-    //ajout des infos au fichier log
-    File.AppendAllText(log, "Fin: " + DateTime.Now + Environment.NewLine);
-    File.AppendAllText(log, SEPARATION + Environment.NewLine);
-    File.AppendAllText(log, "Liste des répertoires concernés : " + Environment.NewLine);
-    foreach (string folder in folders) { File.AppendAllText(log, sourceValue + folder + Environment.NewLine); }
-    File.AppendAllText(log, SEPARATION + Environment.NewLine);
-    File.AppendAllText(log, "Copie différentielle terminée." + "\nTemps d'éxecution : " + (DateTime.Now - dateDebut).ToString(@"hh\:mm\:ss") + Environment.NewLine);
-    File.AppendAllText(log, "Nombre de fichiers mis à jour : " + nbFichiersModifier + Environment.NewLine + "Nombre de fichiers ignorés : " + nbFichierIgnore + Environment.NewLine);
+    Afficher_erreur(nbErreur, log);
+    RemplirLog(dateDebut, log);
 }
 
 //copie différentielle d'un répertoire - copie uniquement les fichiers modifiés
-void CopyDirectoryDiff(string cheminSource, string cheminDestination)
+void CopieDifferentielle(string cheminSource, string cheminDestination)
 {
     //si le répertoire de destination n'existe pas, le créer
     if (!Directory.Exists(cheminDestination))
@@ -188,7 +152,6 @@ void CopyDirectoryDiff(string cheminSource, string cheminDestination)
         //si le fichier n'existe pas dans le répertoire de destination ou si la date de modification est différente du fichier existant dans le répertoire de destination
         if (!File.Exists(Path.Combine(cheminDestination, Path.GetFileName(fichier))) || File.GetLastWriteTime(fichier) != File.GetLastWriteTime(Path.Combine(cheminDestination, Path.GetFileName(fichier))))
         {
-            //copie le fichier dans le répertoire de destination
             File.Copy(fichier, Path.Combine(cheminDestination, Path.GetFileName(fichier)), true);
             nbFichiersModifier += 1;
             Console.WriteLine("Copie de : " + fichier);
@@ -213,12 +176,12 @@ void CopyDirectoryDiff(string cheminSource, string cheminDestination)
         else
         {
             //copie le répertoire et ses sous-répertoires
-            CopyDirectoryDiff(repertoire, Path.Combine(cheminDestination, Path.GetFileName(repertoire)));
+            CopieDifferentielle(repertoire, Path.Combine(cheminDestination, Path.GetFileName(repertoire)));
         }
     }
 }
 
-void CopyDirectory(string cheminSource, string cheminDestination)
+void CopieSimple(string cheminSource, string cheminDestination)
 {
     //si le répertoire de destination n'existe pas, le créer
     if (!Directory.Exists(cheminDestination))
@@ -237,7 +200,7 @@ void CopyDirectory(string cheminSource, string cheminDestination)
     foreach (string repertoire in Directory.GetDirectories(cheminSource))
     {
         //copie le répertoire dans le répertoire de destination
-        CopyDirectory(repertoire, Path.Combine(cheminDestination, Path.GetFileName(repertoire)));
+        CopieSimple(repertoire, Path.Combine(cheminDestination, Path.GetFileName(repertoire)));
     }
 }
 
@@ -257,17 +220,15 @@ void VerifDesFichiers()
             Console.WriteLine("Vérification des fichiers en cours...");
             int NbFichiersVerifier = 0;
 
-            //pour chaque dossier de folder.ini 
-            foreach (string folder in folders)
+            foreach (string dossier in liste_dossier_a_copier)
             {
                 try
                 {
                     //compte le nombre de fichier contenus et dans ses sous-dossiers (AllDirectories)
-                    int NbFichiers = Directory.GetFiles(sourceValue + folder, "*.*", SearchOption.AllDirectories).Length;
-                    Console.WriteLine(sourceValue + folder + " : " + NbFichiers + " fichiers");
+                    int NbFichiers = Directory.GetFiles(repertoireSource + dossier, "*.*", SearchOption.AllDirectories).Length;
+                    Console.WriteLine(repertoireSource + dossier + " : " + NbFichiers + " fichiers");
                     NbFichiersVerifier += NbFichiers;
                 }
-                //erreur si l'accès au dossier n'est pas autorisé
                 catch (Exception e)
                 {
                     Console.WriteLine("Erreur : " + e.Message);
@@ -278,22 +239,19 @@ void VerifDesFichiers()
 
         case 2:
 
-            //pour chaque dossier de folder.ini 
-            foreach (string folder in folders)
+            foreach (string dossier in liste_dossier_a_copier)
             {
                 try
                 {
                     //compte le nombre de fichiers contenus dans les dossiers principaux (TopDirectoryOnly)
-                    int NbFichiers = Directory.GetFiles(sourceValue + folder, "*.*", SearchOption.TopDirectoryOnly).Length;
-                    Console.WriteLine(sourceValue + folder + " : " + NbFichiers + " fichiers");
+                    int NbFichiers = Directory.GetFiles(repertoireSource + dossier, "*.*", SearchOption.TopDirectoryOnly).Length;
+                    Console.WriteLine(repertoireSource + dossier + " : " + NbFichiers + " fichiers");
                 }
-                //erreur si l'accès au dossier n'est pas autorisé
                 catch (Exception e)
                 {
                     Console.WriteLine("Erreur : " + e.Message);
                 }
             }
-            
             break;
 
         default:
@@ -301,6 +259,84 @@ void VerifDesFichiers()
             VerifDesFichiers();
             break;
     }
+}
+
+//supprime les fichiers dans le répertoire de destination qui ne sont pas présents dans le répertoire source
+void SupprimerFichiers(string cheminSource, string cheminDestination)
+{
+    //pour chaque fichier dans le répertoire de destination
+    foreach (string fichier in Directory.GetFiles(cheminDestination))
+    {
+        //si le fichier n'existe pas dans le répertoire source
+        if (!File.Exists(Path.Combine(cheminSource, Path.GetFileName(fichier))))
+        {
+            //supprime le fichier
+            File.Delete(fichier);
+            Console.WriteLine("Suppression de : " + fichier);
+            nbFichierSupprime += 1;
+        }
+    }
+    //pour chaque répertoire dans le répertoire de destination
+    foreach (string repertoire in Directory.GetDirectories(cheminDestination))
+    {
+        //si le répertoire n'existe pas dans le répertoire source
+        if (!Directory.Exists(Path.Combine(cheminSource, Path.GetFileName(repertoire))))
+        {
+            //supprime le répertoire
+            Directory.Delete(repertoire, true);
+            Console.WriteLine("Suppression de : " + repertoire);
+            nbFichierSupprime += 1;
+        }
+        else
+        {
+            //sinon, supprime les fichiers dans le répertoire de destination qui ne sont pas présents dans le répertoire source
+            SupprimerFichiers(Path.Combine(cheminSource, Path.GetFileName(repertoire)), repertoire);
+        }
+    }
+}
+
+void Afficher_erreur(int nbErreur, string log)
+{
+    if (nbErreur > 0)
+    {
+        File.AppendAllText(log, "Nombre d'erreur constatées : " + nbErreur + Environment.NewLine);
+        foreach (string erreur in listeErreur)
+        {
+            File.AppendAllText(log, erreur + Environment.NewLine);
+        }
+    }
+    else
+    {
+        File.AppendAllText(log, "Aucune erreur constatée" + Environment.NewLine);
+    }
+}
+
+void RemplirLog(DateTime dateDebut, string log)
+{
+    Console.WriteLine("\nTache terminee \n\nInformations dans le fichier log : \n" + log);
+
+    //ajout des infos dans le fichier log
+    File.AppendAllText(log, "Fin: " + DateTime.Now + Environment.NewLine);
+    File.AppendAllText(log, SEPARATION + Environment.NewLine);
+    File.AppendAllText(log, "Liste des répertoires concernés : " + Environment.NewLine);
+    foreach (string dossier in liste_dossier_a_copier) { File.AppendAllText(log, repertoireSource + dossier + Environment.NewLine); }
+    File.AppendAllText(log, SEPARATION + Environment.NewLine);
+
+    if (choixMethode == 2)
+    {
+        File.AppendAllText(log, "Copie différentielle terminée : \nNombre de fichiers copiés : " + nbFichiersModifier + "\nNombre de fichiers ignorés : " + nbFichierIgnore);
+    }
+    else
+    {
+        File.AppendAllText(log, "Copie terminée : \nNombre de fichiers copiés : " + nbFichiersModifier);
+    }
+
+    if (suppression_est_demandee)
+    {
+        File.AppendAllText(log, "\nNombre de fichiers supprimés : " + nbFichierSupprime + Environment.NewLine);
+    }
+    
+    File.AppendAllText(log, "\nTemps d'éxecution : " + (DateTime.Now - dateDebut).ToString(@"hh\:mm\:ss") + Environment.NewLine);
 }
 
 void AppuyezPourContinuer()
